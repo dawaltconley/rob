14166fab75bedf26b4c16b3bd2453a1e45f214c2
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2017-02-16 19:53:13 -0500

    improved css-only styles with :target and height transition; may be able to make a css-only thing work pretty well

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
-var preview = document.getElementById("project-view");
 
-var allProjects = document.getElementById("all-projects");
 
-while (allProjects.childNodes.length) {
-    preview.appendChild(allProjects.firstChild);
-}
