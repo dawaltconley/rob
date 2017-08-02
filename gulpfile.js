4b061b5476531482b523712a63fb03312fe02ffb
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2017-08-01 20:00:07 -0400

    installed js gulp packages, added gulpfile, reconfigured some basic parts of site

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var pump = require("pump")
+var child = require("child_process");
+var runSequence = require("run-sequence");
+
+function jekyllBuild(env = "development") {
+    env = "JEKYLL_ENV=" + env + " ";
+    child.execSync(env + "jekyll build", { stdio: "inherit" });
+}
+
+gulp.task("js", function (cb) {
+    pump([
+        gulp.src([
+            "./_site/js/polyfills/rAF.js", // needed for another polyfill
+            "./_site/js/polyfills/*.js",
+            "./_site/js/lib/*.js",
+            "./_site/js/main.js",
+            "!./_site/**/*.min.js",
+            "!./_site/**/*.map"
+        ]),
+        concat("all.js"),
+        uglify(),
+        gulp.dest("./_site/js")
+    ], cb);
+});
+
+gulp.task("deploy", function () {
+    jekyllBuild("production");
+    runSequence("js");
+});
