be122a227a9eedc7635c3784fa221233715c8979
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2017-08-23 05:21:29 -0400

    added drop down menus and sliders for testing bg images

--- js/main.js
+++ js/main.js
@@ -254,6 +254,44 @@
     };
 
 /*
+ * Background Image Testing
+ */
+
+    var menuContainers = toArray(document.querySelectorAll("[data-background-select]"));
+    var bgMenus = [];
+
+    menuContainers.forEach(function (element) {
+        var newMenu = new bgMenu(element);
+        bgMenus.push(newMenu);
+    });
+
+    function bgMenu(element) {
+        this.bgElement = document.getElementById(element.getAttribute("data-background-element"));
+        this.bgElement.classList.add("relative");
+        this.menu = getChildBySelector(element, "select");
+        this.slider = getChildBySelector(element, "input");
+        this.overlay = document.createElement("div");
+        this.overlay.classList.add("fill-parent");
+        this.bgElement.insertBefore(this.overlay, this.bgElement.firstChild);
+    }
+
+    bgMenu.prototype.setBg = function() {
+        var selection = this.menu.selectedOptions[0];
+        var image = selection.value;
+        var darkness = (Number(this.slider.value) / 100).toString();
+        if (selection.hasAttribute("data-position")) {
+            this.bgElement.style.backgroundPosition = selection.getAttribute("data-position");
+        }
+        this.bgElement.style.backgroundImage = "url(" + image + ")";
+        this.overlay.style.backgroundColor = "rgba(0, 0, 0, " + darkness + ")";
+    }
+
+    bgMenus.forEach(function (element) {
+        element.menu.onchange = element.setBg.bind(element);
+        element.slider.onchange = element.setBg.bind(element);
+    });
+
+/*
  * Projects
  */
 
