75df6c0fef578b789228047b6edc827ac378aa25
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2017-08-23 18:40:23 -0400

    standardized bg-image control menus with a jekyll include and better styling / scripting

--- js/main.js
+++ js/main.js
@@ -270,20 +270,19 @@
         this.bgElement.classList.add("relative");
         this.menu = getChildBySelector(element, "select");
         this.slider = getChildBySelector(element, "input");
-        this.overlay = document.createElement("div");
-        this.overlay.classList.add("fill-parent");
-        this.bgElement.insertBefore(this.overlay, this.bgElement.firstChild);
     }
 
     bgMenu.prototype.setBg = function() {
         var selection = this.menu.selectedOptions[0];
         var image = selection.value;
         var darkness = (Number(this.slider.value) / 100).toString();
+        if (selection.hasAttribute("data-size")) {
+            this.bgElement.style.backgroundSize = selection.getAttribute("data-size");
+        }
         if (selection.hasAttribute("data-position")) {
             this.bgElement.style.backgroundPosition = selection.getAttribute("data-position");
         }
-        this.bgElement.style.backgroundImage = "url(" + image + ")";
-        this.overlay.style.backgroundColor = "rgba(0, 0, 0, " + darkness + ")";
+        this.bgElement.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, " + darkness + "), rgba(0, 0, 0, " + darkness + ")), url('" + image + "')";
     }
 
     bgMenus.forEach(function (element) {
